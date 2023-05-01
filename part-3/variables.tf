bc8bf66bc56c7e60dd3e985c7318a5db41985696
Rolf Schutten <74961507+rolfschutten@users.noreply.github.com>
2023-05-01 11:13:19 +0200

    Create variables.tf

---
+++ part-3/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  type    = string
+  default = ""
+}
+
+variable "app_name" {
+  type    = string
+  default = ""
+}
+
+variable "location" {
+  type    = string
+  default = ""
+}
