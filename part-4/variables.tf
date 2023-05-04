cef792b5de39a4550281fa04cc372a93de618ca8
Rolf Schutten <74961507+rolfschutten@users.noreply.github.com>
2023-05-04 07:19:23 +0200

    Update variables.tf

--- part-4/variables.tf
+++ part-4/variables.tf
@@ -10,5 +10,52 @@ variable "app_name" {
 
 variable "location" {
   type    = string
+ default = ""
+}
+
+variable "cdn_profile_name" {
+  type    = string
   default = ""
 }
+
+variable "cdn_endpoint_name" {
+  type    = string
+  default = ""
+}
+
+variable "custom_domain_name" {
+  type    = string
+  default = ""
+}
+
+variable "custom_domain_host_name" {
+  type    = string
+  default = ""
+}
+
+variable "ssl_validation_name" {
+  type    = string
+  default = ""
+}
+
+variable "ssl_validation_record" {
+  type    = string
+  default = ""
+}
+
+variable "keyvault_name" {
+  type    = string
+  default = ""
+}
+
+variable "AzureFrontDoorCdn_ID" {
+  type    = string
+  default = ""
+}
+
+variable "certificate_name" {
+  type    = string
+  default = ""
+}
+
+variable "certificate_password" {}
